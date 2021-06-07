3eaf9fafa431ce173cfc71af1945ae65f722164c
Barack <barackmukelenga100@gmail.com>
2021-06-07 12:41:02 +0200

    Fis the issue with webpack

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,19 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   def index
+  end
+
+  private 
+  def post_params 
+    params.require(:post).permit(:title, :content)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 
 validates :title, presence: true
-validates :content, presence: true, length { maximum: 100 }
+validates :content, presence: true, length: { maximum: 100 }
 
 belongs_to :user
 end
